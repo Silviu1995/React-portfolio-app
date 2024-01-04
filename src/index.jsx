21c492fd2907bb8d000cabb007f4bfd611499cb8
Silviu1995 <bacanaru.silviu@gmail.com>
2024-01-05 01:01:20 +0200

    :updated portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(<React.StrictMode><App /></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
